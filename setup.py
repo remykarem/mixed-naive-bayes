70384ac8df9341eed6d7f958b182b21c4d9b8d18
remykarem <raimi.bkarim@gmail.com>
2022-08-03 21:54:22 +0800

    [CHORE] Update dependencies

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    install_requires=['numpy>=1.16.1', 'scikit-learn>=0.20.2']
+    python_requires='>=3.7',
+    install_requires=['numpy>=1.22.0', 'scikit-learn>=0.20.2']
 )
